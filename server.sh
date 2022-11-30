ea77c86ced9480ea0c5031b9a96c9ac971eeca1a
Samet <sametklou1907@gmail.com>
2022-12-01 01:35:52 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -10,5 +10,9 @@ cd ..
 sudo apt install nodejs -y
 git clone https://github.com/SametBasturkk/RclonePlotMove.git
 mkdir accountservers
-mkdir .config
+mkdir final
+mkdir temp
+mkdir ram
+sudo mount -t tmpfs -o size=110G tmpfs /home/ubuntu/ram
+screen -dmS plotting bash -c './chia-plotter/build/./chia_plot -n -1 -k 32 -r 12 -u 256 -v 256 -f b65f297db130973df365c9aa59a4962a919b9b8cae2b11740095ebf7f68bd0a2054aae15ecdac30158f8d26d2ba02a21 -c xch17v7vumtucgac9hag7exg6llx0g20uqvwazpwkr9mf9st3j82mllqkjnpw6 -t /home/ubuntu/temp/ -2 /home/ubuntu/ram/ -d /home/ubuntu/final/'
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
